61001ba791d2ca094ec795d417e445a5992e5707
Habib-Ashraf-Hassan <100794988+Habib-Ashraf-Hassan@users.noreply.github.com>
2023-06-21 19:58:33 +0300

    added and styled about and contact page

--- index.php
+++ index.php
@@ -6,33 +6,91 @@
     <title>Adan Madrassah</title>
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css">
     <link rel="stylesheet" href="css/style.css">
+    <link rel="icon" href="images/Madrassa_logo2.png">
 </head>
 <body class="body-home">
     <div class="black-fill">
+        <br/>
+        <br/>
         <div class="container">
-        <nav class="navbar navbar-expand-lg bg-body-tertiary">
+        <nav class="navbar navbar-expand-lg bg-body-tertiary" id="homeNav">
   <div class="container-fluid">
-    <a class="navbar-brand" href="#">Navbar</a>
+    <a class="navbar-brand" href="#">
+        <img src="images/Madrassa_logo2.png" alt="Madrassa logo" width="30">
+    </a>
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
     <div class="collapse navbar-collapse" id="navbarSupportedContent">
       <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+
         <li class="nav-item">
           <a class="nav-link active" aria-current="page" href="#">Home</a>
         </li>
+
         <li class="nav-item">
-          <a class="nav-link" href="#">Link</a>
+          <a class="nav-link" href="#about">About</a>
         </li>
-        
+
+        <li class="nav-item">
+          <a class="nav-link" href="#contact">Contact</a>
+        </li>
       </ul>
-      
+      <ul class="navbar-nav me-right mb-2 mb-lg-0">
+        <li class="nav-item">
+            <a class="nav-link" href="#">Log in</a>
+        </li>
+      </ul>
     </div>
   </div>
 </nav>
+    <section class="welcome-text d-flex justify-content-center align-items-center flex-column">
+        <img src="images/Madrassa_logo2.png">
+        <h4> Welcome to Adan Madrassah</h4>
+        <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Quod in enim dolorum itaque eum commodi laborum eligendi obcaecati veniam recusandae accusamus dolore iusto vitae, tenetur exercitationem voluptatibus laboriosam. Possimus, blanditiis.
+        </p>
+
+    </section>
+
+    <section id="about" class="d-flex justify-content-center align-items-center flex-column">
+        <div class="card mb-3 card-1">
+        <div class="row g-0">
+            <div class="col-md-4">
+            <img src="images/Madrassa_logo2.png" class="img-fluid rounded-start" alt="Logo">
+            </div>
+            <div class="col-md-8">
+            <div class="card-body">
+                <h5 class="card-title">About Us</h5>
+                <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                <p class="card-text"><small class="text-body-secondary">Adan Madrassah</small></p>
+            </div>
+            </div>
+        </div>
+        </div>
+    </section>
+
+    <section id="contact" class="d-flex justify-content-center align-items-center flex-column">
+        <form>
+            <h3>Contact Us</h3>
+            <div class="mb-3">
+                <label for="exampleInputEmail1" class="form-label">Email address</label>
+                <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
+                <div id="emailHelp" class="form-text">We'll never share your email with anyone else.</div>
+            </div>
+            <div class="mb-3">
+                <label  class="form-label">Name</label>
+                <input type="text" class="form-control" >
+            </div>
+            <div class="mb-3 form-check">
+                <input type="checkbox" class="form-check-input" id="exampleCheck1">
+                <label class="form-check-label" for="exampleCheck1">Check me out</label>
+            </div>
+            <button type="submit" class="btn btn-primary">Submit</button>
+        </form>
+    </section>
 
         </div>
-
     </div>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js">
     </script>
